9d979a37f53b7e37473f843b14ffbf1abee76cf9
tkalayci71 <114084637+tkalayci71@users.noreply.github.com>
2023-01-29 23:49:25 +0300

    v0.2

--- scripts/mini_diffusion.py
+++ scripts/mini_diffusion.py
@@ -1,47 +1,21 @@
 #
 # https://github.com/tkalayci71/mini-diffusion
 #
-# mini_diffusion version 0.1
+# mini_diffusion version 0.2
 #
 
 import torch
 import scripts.mini_diffusion_utils as mdu
-#import importlib
-#importlib.reload(mdu)
+import importlib
+importlib.reload(mdu)
 
 #-------------------------------------------------------------------------------
 
-def do_generate(prompt,negative_prompt,seed,cfg_scale,steps,width,height):
-
-    sd_version = mdu.get_sd_version()
-    if (sd_version!=1): return None
-
-    unet =mdu.get_unet()
-    torch_device, data_type, device_type = mdu.get_model_info(unet)
-    scheduler = mdu.mini_lms()
-
-    text_embeddings = mdu.text_list_to_embeddings([negative_prompt,prompt])
-    latents = mdu.generate_random_latents(seed,width,height,1,torch_device,data_type,generator_device='cpu')
-    scheduler.set_timesteps(steps, torch_device)
-    latents = latents * scheduler.init_noise_sigma
-
-    with torch.autocast(device_type=device_type, dtype=data_type):
-        for i, t in enumerate(scheduler.timesteps):
-            print('step = ',i)
-            latent_model_input = torch.cat([latents] * 2)
-            sigma = scheduler.sigmas[i]
-            latent_model_input = scheduler.scale_model_input(latent_model_input, t)
-            tt = torch.tensor([t,t]).to(device=torch_device,dtype=data_type)
-            with torch.no_grad():
-                noise_pred = unet.forward(latent_model_input, timesteps=tt, context = text_embeddings)
-            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-            noise_pred = noise_pred_uncond + cfg_scale * (noise_pred_text - noise_pred_uncond)
-            latents = scheduler.step(noise_pred, t, latents)
-
-    images = mdu.latents_to_pil_image(latents)
-    result_image = images[0]
-
-    return result_image
+def do_generate(prompt,negative_prompt,seed,cfg_scale,steps,width,height,rand_cpu):
+    prompts = prompt.split('\n')
+    randgen_device='cpu' if rand_cpu else None
+    result_images, result_text = mdu.prompts_to_images(prompts,negative_prompt,seed,cfg_scale,steps,width,height,randgen_device=randgen_device)
+    return result_images, result_text
 
 #-------------------------------------------------------------------------------
 
@@ -53,24 +27,30 @@ def add_tab():
         with gr.Row():
             with gr.Column(scale=1):
                 with gr.Row():
-                    prompt = gr.Textbox(label='Prompt',value='A watercolor painting of an otter')
+                    prompt = gr.Textbox(label='Prompt',value='A watercolor painting of an otter',lines=2)
                 with gr.Row():
                     negative_prompt = gr.Textbox(label='Negative Prompt')
                 with gr.Row():
-                    seed = gr.Number(label='Seed',value=32)
-                    cfg_scale = gr.Slider(label='CFG Scale',value=7.5,minimum=1,maximum=25,step=0.5)
-                    steps = gr.Slider(label='Steps',value=30,minimum=1,maximum=50,step=1)
+                    with gr.Column():
+                        seed = gr.Number(label='Seed',value=32)
+                        rand_cpu = gr.Checkbox(label='CPU random generator',value=True)
+                    with gr.Column():
+                        cfg_scale = gr.Slider(label='CFG Scale',value=7.5,minimum=0,maximum=15,step=0.5)
+                        steps = gr.Slider(label='Steps',value=30,minimum=0,maximum=50,step=1)
                 with gr.Row():
                     width = gr.Slider(label='Width',value=512,minimum=256,maximum=768,step=64)
                     height = gr.Slider(label='Height',value=512,minimum=256,maximum=768,step=64)
                 with gr.Row():
                     generate = gr.Button("Generate",variant='primary')
+                with gr.Row():
+                    result_text = gr.Textbox(show_label=False,interactive=False,lines=2,maxlines=2)
+
             with gr.Column(scale=1):
-                result_image = gr.Image(show_label=False,interactive=False)
+                result_image = gr.Gallery(show_label=False,interactive=False)
 
         generate.click(fn=do_generate,
-            inputs=[prompt,negative_prompt,seed,cfg_scale,steps,width,height],
-            outputs=[result_image])
+            inputs=[prompt,negative_prompt,seed,cfg_scale,steps,width,height,rand_cpu],
+            outputs=[result_image,result_text])
 
     return [(ui, "Mini-Diffusion", "Mini_Diffusion")]
 
--- scripts/mini_diffusion_utils.py
+++ scripts/mini_diffusion_utils.py
@@ -1,7 +1,7 @@
 #
 # https://github.com/tkalayci71/mini-diffusion
 #
-# mini_diffusion_utils version 0.1
+# mini_diffusion_utils version 0.2
 #
 
 from modules import shared
@@ -9,6 +9,8 @@ import torch
 import numpy as np
 from scipy import integrate
 from PIL import Image
+from time import perf_counter
+from math import ceil
 
 #-------------------------------------------------------------------------------
 
@@ -63,6 +65,7 @@ def text_list_to_embeddings(text_list):
     torch_device, data_type, device_type = get_model_info(text_encoder)
 
     text_input = tokenizer(text_list, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
+    #text_input = tokenizer(text_list, padding="longest", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
     input_ids = text_input.input_ids.to(torch_device)
     with torch.autocast(device_type=device_type, dtype=data_type):
         with torch.no_grad():
@@ -75,7 +78,8 @@ def generate_random_latents(seed,width,height,batch_size, torch_device, data_typ
     if generator_device==None: generator_device=torch_device
     generator = torch.Generator(generator_device)
     generator.manual_seed(int(seed))
-    latents = torch.randn((batch_size, 4, height // 8, width // 8),generator=generator,device=generator_device)
+    latents = torch.randn((1, 4, height // 8, width // 8),generator=generator,device=generator_device)
+    latents = torch.cat([latents]*batch_size)
     latents = latents.to(device=torch_device,dtype=data_type)
     return latents
 
@@ -125,7 +129,8 @@ class mini_lms:
         timestep = timestep.to(self.timesteps.device)
         step_index = (self.timesteps == timestep).nonzero().item()
         sigma = self.sigmas[step_index]
-        sample = sample / ((sigma**2 + 1) ** 0.5)
+        divval = ((sigma**2 + 1) ** 0.5)
+        sample = sample / divval
         return sample
 
     def get_lms_coefficient(self, order, t, current_order):
@@ -139,9 +144,9 @@ class mini_lms:
         integrated_coeff = integrate.quad(lms_derivative, self.sigmas[t], self.sigmas[t + 1], epsrel=1e-4)[0]
         return integrated_coeff
 
-    def step(self, model_output: torch.FloatTensor,  timestep, sample: torch.FloatTensor,order: int = 4) :
+    def step(self, model_output: torch.FloatTensor, step_index, timestep, sample: torch.FloatTensor,order: int = 4) :
         timestep = timestep.to(self.timesteps.device)
-        step_index = (self.timesteps == timestep).nonzero().item()
+        #step_index = (self.timesteps == timestep).nonzero().item()
         sigma = self.sigmas[step_index]
         pred_original_sample = sample - sigma * model_output
         derivative = (sample - pred_original_sample) / sigma
@@ -151,6 +156,62 @@ class mini_lms:
         lms_coeffs = [self.get_lms_coefficient(order, step_index, curr_order) for curr_order in range(order)]
         prev_sample = sample + sum(coeff * derivative for coeff, derivative in zip(lms_coeffs, reversed(self.derivatives)))
         return prev_sample
+
+#-------------------------------------------------------------------------------
+
+def prompts_to_images(prompts,negative_prompt,seed,cfg_scale,steps,width,height,randgen_device=None):
+
+    sd_version = get_sd_version()
+    if (sd_version!=1): return None, 'Error: SD1 only'
+
+    batch_size = len(prompts)
+
+    log = []
+
+    total_clock_start = perf_counter()
+
+    clock_start = perf_counter()
+    unet =get_unet()
+    torch_device, data_type, device_type = get_model_info(unet)
+    if randgen_device==None:
+        randgen_device = torch_device
+
+    scheduler = mini_lms()
+    positive_embeddings = text_list_to_embeddings(prompts)
+    negative_embeddings = text_list_to_embeddings([negative_prompt]*batch_size)
+    text_embeddings = torch.cat([negative_embeddings,positive_embeddings])
+    latents = generate_random_latents(seed,width,height,batch_size,torch_device,data_type,generator_device=randgen_device)
+    scheduler.set_timesteps(steps,'cpu')
+    latents = latents * scheduler.init_noise_sigma
+    clock_stop = perf_counter()
+    log.append('prep time : '+str(ceil(clock_stop*1000-clock_start*1000))+' ms')
+
+    clock_start = perf_counter()
+    with torch.autocast(device_type=device_type, dtype=data_type):
+        for i, t in enumerate(scheduler.timesteps):
+            #print('step = ',i)
+            latent_model_input = torch.cat([latents] * 2)
+            sigma = scheduler.sigmas[i]
+            latent_model_input = scheduler.scale_model_input(latent_model_input, t)
+            tt = torch.tensor([t]*2*batch_size).to(device=torch_device,dtype=data_type)
+            with torch.no_grad():
+                noise_pred = unet.forward(latent_model_input, timesteps=tt, context = text_embeddings)
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + cfg_scale * (noise_pred_text - noise_pred_uncond)
+            latents = scheduler.step(noise_pred, i, t, latents)
+    clock_stop = perf_counter()
+    log.append('diffusion time : '+str(ceil(clock_stop*1000-clock_start*1000))+' ms')
+
+    clock_start = perf_counter()
+    result_images = latents_to_pil_image(latents)
+    clock_stop = perf_counter()
+    log.append('vae decode time: '+str(ceil(clock_stop*1000-clock_start*1000))+' ms')
+
+    total_clock_stop = perf_counter()
+    log.append('total time : '+str(ceil(total_clock_stop*1000-total_clock_start*1000))+' ms')
+
+    result_text = '\n'.join(log)
+    return result_images, result_text
 
 #-------------------------------------------------------------------------------
 
